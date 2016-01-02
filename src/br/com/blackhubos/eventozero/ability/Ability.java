d861a42c9dc0251e0901aa38c0163e178ea875d9
Acer <Acer@Acer-PC>
2016-01-01 23:44:44 -0200

    commited

---
+++ src/br/com/blackhubos/eventozero/ability/Ability.java
@@ -0,0 +1,29 @@
+package br.com.blackhubos.eventozero.ability;
+
+import org.bukkit.entity.Player;
+
+public abstract class Ability {
+	
+	private long cooldown;
+	private long lastTime;
+	
+	public Ability(long cooldown){
+		this.cooldown = cooldown;
+		this.lastTime = 0;
+	}
+	
+	public long getRemaingTime(){
+		return (lastTime - (cooldown * 1000));
+	}
+	
+	public long getRemaingTimePostive(){
+		return (getRemaingTime() >= 0? getRemaingTime() : 0);
+	}
+	
+	public abstract boolean canUse();
+	
+	public abstract boolean tryUse(Player player);
+	
+	public abstract void foceUse(Player player);
+	
+}
