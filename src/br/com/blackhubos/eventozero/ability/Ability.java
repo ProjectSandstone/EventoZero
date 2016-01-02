9af1f3b7d484b7cf853ab94cc25cf801a13328d2
Pitter Thog <blackthog@outlook.com>
2016-01-02 14:33:51 -0200

    Classes formatadas, nenhuma mudança alheia

--- src/br/com/blackhubos/eventozero/ability/Ability.java
+++ src/br/com/blackhubos/eventozero/ability/Ability.java
@@ -1,7 +1,7 @@
 /**
  *
  * EventoZero - Advanced event factory and executor for Bukkit and Spigot.
- * Copyright © 2016 BlackHub OS and contributors.
+ * Copyright ï¿½ 2016 BlackHub OS and contributors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,30 +21,35 @@ package br.com.blackhubos.eventozero.ability;
 
 import org.bukkit.entity.Player;
 
-public abstract class Ability {
-	
-	private long cooldown;
-	private long lastTime;
-	
-	public Ability(long cooldown){
+public abstract class Ability
+{
+
+	private final long cooldown;
+	private final long lastTime;
+
+	public Ability(final long cooldown)
+	{
 		this.cooldown = cooldown;
 		this.lastTime = 0;
 	}
-	
-	public long getRemaingTime(){
-		return (lastTime - (cooldown * 1000));
+
+	public long getRemaingTime()
+	{
+		return (this.lastTime - (this.cooldown * 1000));
 	}
-	
-	public long getRemaingTimePostive(){
-		return (getRemaingTime() >= 0? getRemaingTime() : 0);
+
+	public long getRemaingTimePostive()
+	{
+		return (this.getRemaingTime() >= 0 ? this.getRemaingTime() : 0);
 	}
-	
-	public boolean canUse(){
-		return (getRemaingTimePostive() == 0);
+
+	public boolean canUse()
+	{
+		return (this.getRemaingTimePostive() == 0);
 	}
-	
+
 	public abstract boolean tryUse(Player player);
-	
+
 	public abstract void foceUse(Player player);
-	
+
 }
--- src/br/com/blackhubos/eventozero/ability/abilitys/DoubleJump.java
+++ src/br/com/blackhubos/eventozero/ability/abilitys/DoubleJump.java
@@ -1,47 +1,51 @@
 /**
-*
-* EventoZero - Advanced event factory and executor for Bukkit and Spigot.
-* Copyright © 2016 BlackHub OS and contributors.
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*
-*/
+ *
+ * EventoZero - Advanced event factory and executor for Bukkit and Spigot.
+ * Copyright ï¿½ 2016 BlackHub OS and contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 package br.com.blackhubos.eventozero.ability.abilitys;
 
 import org.bukkit.entity.Player;
 
 import br.com.blackhubos.eventozero.ability.Ability;
 
-public class DoubleJump extends Ability{
+public final class DoubleJump extends Ability
+{
 
-	public DoubleJump(long cooldown) {
+	public DoubleJump(final long cooldown)
+	{
 		super(cooldown);
-		
 	}
 
 	@Override
-	public boolean tryUse(Player player) {
-		if(canUse()){
-			foceUse(player);
+	public boolean tryUse(final Player player)
+	{
+		if (this.canUse())
+		{
+			this.foceUse(player);
 			return true;
 		}
 		return false;
 	}
 
 	@Override
-	public void foceUse(Player player) {
-		player.setVelocity(player.getLocation().getDirection().multiply(0.5).setY(1));	
+	public void foceUse(final Player player)
+	{
+		player.setVelocity(player.getLocation().getDirection().multiply(0.5).setY(1));
 	}
 
 }
