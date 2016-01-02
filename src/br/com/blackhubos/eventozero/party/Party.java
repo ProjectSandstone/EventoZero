da884295afdf4c2aabd8728943e424b9890820c6
Pitter Thog <blackthog@outlook.com>
2016-01-02 14:37:46 -0200

    A classe foi tornada final e formatada.

--- src/br/com/blackhubos/eventozero/party/Party.java
+++ src/br/com/blackhubos/eventozero/party/Party.java
@@ -1,7 +1,7 @@
 /**
  *
  * EventoZero - Advanced event factory and executor for Bukkit and Spigot.
- * Copyright © 2016 BlackHub OS and contributors.
+ * Copyright ï¿½ 2016 BlackHub OS and contributors.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -23,46 +23,55 @@ import java.util.Vector;
 
 import org.bukkit.entity.Player;
 
-public class Party {
-	
+public final class Party
+{
+
 	private final Vector<Player> joineds;
 	private final Vector<Player> inviteds;
 	private final int max;
-	
-	public Party(int max){
+
+	public Party(final int max)
+	{
 		this.joineds = new Vector<>();
 		this.inviteds = new Vector<>();
 		this.max = max;
 	}
-	
-	public Player getOwner(){
+
+	public Player getOwner()
+	{
 		return this.joineds.get(0);
 	}
-	
-	public Vector<Player> getJoineds(){
+
+	public Vector<Player> getJoineds()
+	{
 		return this.joineds;
 	}
-	
-	public Vector<Player> getInviteds(){
+
+	public Vector<Player> getInviteds()
+	{
 		return this.inviteds;
 	}
-	
-	public Party playerJoin(Player player){
+
+	public Party playerJoin(final Player player)
+	{
 		this.joineds.add(player);
 		return this;
 	}
-	
-	public Party playerQuit(Player player){
+
+	public Party playerQuit(final Player player)
+	{
 		this.joineds.remove(player);
 		return this;
 	}
-	
-	public Party playerJoinInvited(Player player){
+
+	public Party playerJoinInvited(final Player player)
+	{
 		this.inviteds.addElement(player);
 		return this;
 	}
-	
-	public Party playerQuitInvited(Player player){
+
+	public Party playerQuitInvited(final Player player)
+	{
 		this.inviteds.remove(player);
 		return this;
 	}
