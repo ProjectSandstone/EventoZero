9e2cbe0b8e7eb0ce60b0504044aaa75dec04fbd5
Pitter Thog <blackthog@outlook.com>
2016-01-03 21:01:44 -0200

    KitHandler e ShopHandler foram colocadas no pacote 'handlers' e foi aplicado a licença nelas; código formatado também.

---
+++ src/br/com/blackhubos/eventozero/handlers/ShopHandler.java
@@ -0,0 +1,63 @@
+/**
+ *
+ * EventoZero - Advanced event factory and executor for Bukkit and Spigot.
+ * Copyright © 2016 BlackHub OS and contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package br.com.blackhubos.eventozero.handlers;
+
+import java.io.File;
+import java.util.Vector;
+
+import org.bukkit.plugin.Plugin;
+
+import br.com.blackhubos.eventozero.EventoZero;
+import br.com.blackhubos.eventozero.shop.Shop;
+import br.com.blackhubos.eventozero.util.Framework.Configuration;
+import br.com.blackhubos.eventozero.util.Framework.ItemFactory;
+
+public class ShopHandler
+{
+
+	private final Vector<Shop> shops;
+
+	public ShopHandler()
+	{
+		this.shops = new Vector<>();
+	}
+
+	public void loadShops(final Plugin plugin)
+	{
+		final File file = new File(plugin.getDataFolder() + File.separator + "shop" + File.separator + "shops.yml");
+		final Configuration configuration = new Configuration(file);
+		for (final String key : configuration.getConfigurationSection("shops").getKeys(false))
+		{
+			final Shop shop = new Shop(configuration.getString("shops." + key + ".name"), new ItemFactory(configuration.getString("shops." + key + ".icon")).getItem());
+			for (final String s : configuration.getStringList("shops." + key + ".items"))
+			{
+				if (EventoZero.getKitHandler().getKitByName(s.replace("{|}", "")) != null)
+				{
+					shop.addItem(EventoZero.getKitHandler().getKitByName(s.replace("{|}", "")));
+				}
+				else
+				{
+
+				}
+			}
+		}
+	}
+
+}
