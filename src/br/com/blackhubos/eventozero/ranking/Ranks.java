5c109ea40f2d667260f052ed2f5392d69ab5127f
Pitter Thog <blackthog@outlook.com>
2016-01-03 00:47:14 -0200

    Removido código antigo obsoleto e adicionado declaração de licença.

--- src/br/com/blackhubos/eventozero/ranking/Ranks.java
+++ src/br/com/blackhubos/eventozero/ranking/Ranks.java
@@ -1,14 +1,25 @@
+/**
+ *
+ * EventoZero - Advanced event factory and executor for Bukkit and Spigot.
+ * Copyright © 2016 BlackHub OS and contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 package br.com.blackhubos.eventozero.ranking;
 
-import br.com.blackhubos.eventozero.ConfigFiles;
-
-public class Ranks {
-	
-	 ConfigFiles config = ConfigFiles.getInstance();
-	 
-	 public void teste() {
-		 config.getMensagem().getString("Teste");
-		 config.getRank().getString("Teste");
-	 }
+public final class Ranks
+{
 
 }
