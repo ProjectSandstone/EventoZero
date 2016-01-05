5b3ba8bec36aae5124c6d434f9500863f48b9064
CrazyDev <hugoleonardo_08@hotmail.com>
2016-01-05 11:07:49 -0200

    O que foi alterado: Classe criada
    O que foi corrigido:

---
+++ src/br/com/blackhubos/eventozero/commands/CommandStart.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.blackhubos.eventozero.commands;
+
+import br.com.blackhubos.eventozero.EventCommand;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+/**
+ *
+ * @author Hugo
+ */
+public class CommandStart extends EventCommand {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        
+        return false;
+    }
+
+}
