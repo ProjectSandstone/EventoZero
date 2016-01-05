3cc7b5db6d4b4a11a959ecaae5ccfcd09c476404
CrazyDev <hugoleonardo_08@hotmail.com>
2016-01-05 11:01:47 -0200

    O que foi alterado: Adicionado método onBuyed(Event event, Player player);
    O que foi corrigido:
    NADA PARA SER CORRIGIDO

--- src/br/com/blackhubos/eventozero/shop/ShopItem.java
+++ src/br/com/blackhubos/eventozero/shop/ShopItem.java
@@ -1,41 +1,48 @@
 package br.com.blackhubos.eventozero.shop;
 
+import br.com.blackhubos.eventozero.EventoZero;
 import org.bukkit.inventory.ItemStack;
 
 import br.com.blackhubos.eventozero.factory.Event;
+import org.bukkit.entity.Player;
 
 public class ShopItem {
-	
-	private final String name;
-	private final ItemStack itemIcon;
-	
-	private int pricePoints;
-	
-	public ShopItem(String name, ItemStack icon){
-		this.name = name;
-		this.itemIcon = icon;
-		this.pricePoints = 0;
-	}
-	
-	public String getName(){
-		return this.name;
-	}
-	
-	public ItemStack getIcon(){
-		return this.itemIcon;
-	}
-	
-	public int getPrice(){
-		return this.pricePoints;
-	}
-	
-	public ShopItem updatePrice(final int v){
-		this.pricePoints = v;
-		return this;
-	}
-	
-	public ShopItem onBuyed(Event event){
-		return this;
-	}
+
+    private final String name;
+    private final ItemStack itemIcon;
+
+    private int pricePoints;
+
+    public ShopItem(String name, ItemStack icon) {
+        this.name = name;
+        this.itemIcon = icon;
+        this.pricePoints = 0;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public ItemStack getIcon() {
+        return this.itemIcon;
+    }
+
+    public int getPrice() {
+        return this.pricePoints;
+    }
+
+    public boolean equals(ItemStack stack) {
+        return (itemIcon.equals(stack));
+    }
+
+    public ShopItem updatePrice(final int v) {
+        this.pricePoints = v;
+        return this;
+    }
+
+    public ShopItem onBuyed(Event event, Player player) {
+        EventoZero.getStorage().withdrawPlayerRankingPoints("atoM", "spleef", "vitorias", 1);
+        return this;
+    }
 
 }
