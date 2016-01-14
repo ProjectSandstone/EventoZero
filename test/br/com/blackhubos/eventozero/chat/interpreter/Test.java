27040d7211cb874301f093f7919e4be0078cd5be
Jonathan <joniweb01@gmail.com>
2016-01-14 01:59:01 -0200

    Pequena atualização no Test

--- test/br/com/blackhubos/eventozero/chat/interpreter/Test.java
+++ test/br/com/blackhubos/eventozero/chat/interpreter/Test.java
@@ -1,3 +1,22 @@
+/**
+ *
+ * EventoZero - Advanced event factory and executor for Bukkit and Spigot.
+ * Copyright © 2016 BlackHub OS and contributors.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 package br.com.blackhubos.eventozero.chat.interpreter;
 
 import java.util.Arrays;
@@ -5,9 +24,6 @@ import java.util.Arrays;
 import br.com.blackhubos.eventozero.chat.interpreter.pattern.Patterns;
 import static org.junit.Assert.assertEquals;
 
-/**
- * Created by jonathan on 12/01/16.
- */
 public class Test {
 
     @org.junit.Test
